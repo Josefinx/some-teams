0fa54bae7e3b6c29fc0485b0057f0198b329abdc
Josefinx <josefin.wetterberg@gmail.com>
2024-10-07 12:53:01 +0200

    added grid-container and grid-items

--- index.php
+++ index.php
@@ -3,20 +3,23 @@ require __DIR__ . '/header.php';
 require __DIR__ . '/data.php';
 ?>
 <main>
-    <?php
-    foreach ($teams as $key => $value) {
-    ?>
-        <h1><?= $key; ?></h1>
-        <img src="<?= $value['logo']; ?>" alt="logo">
-        <p><?= $value['city']; ?></p>
-        <p><?= $value['league']; ?></p>
-        <p><?= $value['uefa-coefficient-ranking']; ?></p>
-        <p><?= $value['group']; ?></p>
-        <a href="<?= $value['url']; ?>">Mer information om <?= $key ?> </a>
-
-    <?php
-    }
-    ?>
+    <div class="grid-container">
+        <?php
+        foreach ($teams as $key => $value) {
+        ?>
+            <div class="grid-item">
+                <h1><?= $key; ?></h1>
+                <img src="<?= $value['logo']; ?>" alt="logo">
+                <p><?= $value['city']; ?></p>
+                <p><?= $value['league']; ?></p>
+                <p><?= $value['uefa-coefficient-ranking']; ?></p>
+                <p><?= $value['group']; ?></p>
+                <a href="<?= $value['url']; ?>">Mer information om <?= $key ?> </a>
+            </div>
+        <?php
+        }
+        ?>
+    </div>
 </main>
 <?php
 require __DIR__ . '/footer.php';
