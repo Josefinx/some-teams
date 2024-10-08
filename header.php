742775ecab9efd4fa78a0145a57c23d34ef7bd82
Josefinx <josefin.wetterberg@gmail.com>
2024-10-08 18:07:22 +0200

    changed css-link

--- header.php
+++ header.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="/style.css">
+    <title>Some teams</title>
+    <link rel="stylesheet" href="style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
@@ -13,8 +13,8 @@
 
 <body>
     <header>
-        <a href="/index.php"><img class="start" src="/assets/logo_light_blue.svg" alt=""></a>
+        <a href="./index.php"><img class="start" src="./assets/logo_light_blue.svg" alt=""></a>
         <nav>
-            <a class="about" href="/about.php">Om</a>
+            <a class="about" href="./about.php">Om</a>
         </nav>
     </header>
